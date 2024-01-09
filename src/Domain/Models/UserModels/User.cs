75254a6bf9198a759c44b5d0a65f377064c270d3
Sheymor <joseacvz81@gmail.com>
2024-01-08 20:18:57 -0400

    feat: added Postgres database

---
+++ src/Domain/Models/UserModels/User.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Models.UserModels;
+
+public class User
+{
+    public string UserId { get; set; }
+    public string Dni { get; set; }
+    public string Name { get; set; }
+    public string LastName { get; set; }
+    public string NickName { get; set; }
+    public string Password { get; set; }
+}
---
+++ src/Infrastructure/Context/DbContext.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Models.UserModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Context;
+
+public class DatabaseContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+
+    public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>().HasKey(x => x.UserId);
+    }
+}
